c95ecb45c11cb46c562311890ef282c077cd8fe7
MRINMOYBORDOLOI <2105039@kiit.ac.in>
2023-07-14 00:06:15 +0530

    Metaverse app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { ChatIcon } from './svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the front end</h1>
+      <ChatIcon className="red"/>
+    </div>
+  );
+}
+
+export default App;
